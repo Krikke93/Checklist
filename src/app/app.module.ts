8a87ea6f193708d542d4508b11ac918a678ec5c7
Kristof De Langhe <krikke93@hotmail.com>
2018-09-18 20:34:32 +0200

    basic hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { GroupComponent } from './category/group/group.component';
+import { ItemComponent } from './category/group/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    GroupComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
