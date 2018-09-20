b19528f8c1b811335d22aa03e7333b61dd6bec9d
Kristof De Langhe <krikke93@hotmail.com>
2018-09-20 16:58:37 +0200

    improved storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { Category } from 'src/app/category/category.model';
-import { Item } from 'src/app/category/group/item/item.model';
 import { environment } from '../environments/environment';
 import { CategoryService } from 'src/app/category/category.service';
-import { Observable, of } from 'rxjs';
-import { LOCAL_STORAGE, WebStorageService, StorageService } from 'angular-webstorage-service';
-import { Inject } from '@angular/core';
 import { OnDestroy } from '@angular/core';
 import { HostListener } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -20,20 +15,14 @@ export class AppComponent implements OnDestroy, OnInit {
   game = environment.game;
   categories: Category[];
 
-  STORE_KEY = this.game + '_categories';
-
   constructor(
-    private categoryService: CategoryService, 
-    @Inject(LOCAL_STORAGE) private storage: StorageService
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
     this.categoryService.getJSON().subscribe((categories) => {
       this.categories = categories;
-      const storeCategories = JSON.parse(this.storage.get(this.STORE_KEY));
-      if(storeCategories) {
-        this.categories = Object.assign(categories, storeCategories);
-      }
+      this.categoryService.loadAll(this.categories);
     });
   }
 
@@ -47,12 +36,12 @@ export class AppComponent implements OnDestroy, OnInit {
   }
 
   reset() {
-    this.storage.remove(this.STORE_KEY);
+    this.categoryService.eraseAll(this.categories);
     this.ngOnInit();
   }
 
   store() {
-    this.storage.set(this.STORE_KEY, JSON.stringify(this.categories));
+    this.categoryService.storeAll(this.categories);
   }
 
 }
--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { Group } from 'src/app/category/group/group.model';
 import { Category } from 'src/app/category/category.model';
+import { LOCAL_STORAGE, StorageService } from 'angular-webstorage-service';
+import { Item } from './group/item/item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, @Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
   public getJSON(): Observable<any> {
     return this.http.get(`../../assets/hierarchy/${environment.game}/hierarchy.json`).pipe(
@@ -32,10 +34,61 @@ export class CategoryService {
         if(!this.categoryAllChecked(subCategory)) return false;
       }
     }
-    for(let group of category.groups) {
-      if(!this.groupAllChecked(group)) return false;
+    if(category.groups && category.groups.length > 0) {
+      for(let group of category.groups) {
+        if(!this.groupAllChecked(group)) return false;
+      }
     }
     return true;
+  }
+
+  public getAllItems(categories: Category[]): Item[] {
+    let items = [];
+    for(let category of categories) {
+      items = [ ...items, ...this.getAllItemsFromCategory(category)];
+    }
+    return items;
+  }
+
+  public getAllItemsFromCategory(category: Category): Item[] {
+    let items = [];
+    if(category.subCategories && category.subCategories.length > 0) {
+      for(let subCategory of category.subCategories) {
+        items = [ ...items, ...this.getAllItemsFromCategory(subCategory)];
+      }
+    }
+    if(category.groups && category.groups.length > 0) {
+      for(let group of category.groups) {
+        items = [ ...items, ...group.items];
+      }
+    }
+    return items;
+  }
+
+  public storeAll(categories: Category[]) {
+    const items = this.getAllItems(categories);
+    for(let item of items) {
+      this.storage.set(this.getStorageKey(item), item.checked);
+    }
+  }
+
+  private getStorageKey(item: Item): string {
+    return `${environment.game}_checked_${item.src}`;
+  }
+
+  public loadAll(categories: Category[]) {
+    const items = this.getAllItems(categories);
+    for(let item of items) {
+      const storedValue = this.storage.get(this.getStorageKey(item));
+      item.checked = storedValue;
+    }
+  }
+
+  public eraseAll(categories: Category[]) {
+    const items = this.getAllItems(categories);
+    for(let item of items) {
+      this.storage.remove(this.getStorageKey(item));
+    }
   }
 
 }
